835ef5adde45719e454f13a88631c8481064f75a
Mellieejj <Melanieverheul1992@gmail.com>
2021-01-24 16:49:35 +0100

    add some type, indenting and formating files

--- src/app/starter-pokemons-list/starter-pokemons-list.component.ts
+++ src/app/starter-pokemons-list/starter-pokemons-list.component.ts
@@ -6,47 +6,46 @@ import { PokemonsService } from '../services/pokemons.service';
   templateUrl: './starter-pokemons-list.component.html',
   styleUrls: ['./starter-pokemons-list.component.scss'],
 })
+
 export class StarterPokemonsListComponent implements OnInit {
-  starterPokemons: any[] = [];
   regions: any[] = [];
 
   constructor(private PokemonsService: PokemonsService) {}
 
   ngOnInit(): void {
     this.getRegionsData();
-    
   }
-  
-  getRegionsData() {
+
+  getRegionsData():void {
     this.PokemonsService.getGenerationList().subscribe((response: any) => {
       response.results.forEach((generation: any) => {
-        const id = generation.url.slice(
+        const id:number = generation.url.slice(
           generation.url.length - 2,
           generation.url.length - 1
-          );
-          this.PokemonsService.getGenerationPokemons(id).subscribe(
-            (generation: any) => {
-              this.regions = [
-                ...this.regions,
-                {
-                  region: generation['main_region'].name,
-                  pokemons: generation['pokemon_species'].slice(0, 3),
-                },
-              ];
+        );
 
-             this.regions = this.regions.map(region => {
-                const pokemons = region.pokemons
-                let pokemonsData = []
-                pokemons.forEach(p => this.PokemonsService.getPokemonData(p.name).subscribe(data => pokemonsData.push(data)
+        this.PokemonsService.getGenerationPokemons(id).subscribe(
+          (generation: any) => {
+            this.regions = [
+              ...this.regions,
+              {
+                region: generation['main_region'].name,
+                pokemons: generation['pokemon_species'].slice(0, 3),
+              },
+            ];
 
-                ))
-                
-                return {...region, pokemonsData}
-              })
-              
-             }
-            );
-            
+            this.regions = this.regions.map((region: any) => {
+              const pokemonsData: any[] = [];
+              region.pokemons.forEach((p: any) =>
+                this.PokemonsService.getPokemonData(p.name).subscribe((data: any) =>
+                  pokemonsData.push(data)
+                )
+              );
+
+              return { ...region, pokemonsData: pokemonsData };
+            });
+          }
+        );
       });
     });
   }
