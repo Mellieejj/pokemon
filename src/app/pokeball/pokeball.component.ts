0c8bcd9ed29318b99e1c261bb77316c88dcd4374
Mellieejj <Melanieverheul1992@gmail.com>
2021-01-30 16:33:06 +0100

    create pokeball component, add content to home component, update some indenting

---
+++ src/app/pokeball/pokeball.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokeball',
+  templateUrl: './pokeball.component.html',
+  styleUrls: ['./pokeball.component.scss'],
+})
+
+export class PokeballComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
